087f6da5e3e6379f493f1ab16932dbd832128040
Gustavo Santos <dgustavopaula@gmail.com>
2026-09-19 15:19:37 -0300

    cleanup: remover endpoints locais e Render do Console

--- docs/js/admin-control.js
+++ docs/js/admin-control.js
@@ -41,7 +41,7 @@
   function apiBase() {
     return typeof SalaDeComando !== "undefined"
       ? SalaDeComando.apiBase
-      : "https://nexoterracore-api.onrender.com";
+      : "https://api.gustavopaulasantos.com.br";
   }
 
   async function api(caminho, opcoes = {}) {
